2537bbe1dc17b4283c996bfc3b2b401a12ea94fb
n33lp <116659586+n33lp@users.noreply.github.com>
2025-01-28 18:12:08 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,33 @@
+# **AI Chatbot**
+
+This is a simple AI chatbot built using Python, LangChain, and Chainlit. The chatbot is powered by the Phi-3-Mini-4K-Instruct model, which is exposed through the Hugging Face framework. This chatbot maintains context during a user session making it a stateful conversational AI agent.
+
+## Tech Stack
+- Backend: Python, LangChain
+- Frontend: Chainlit
+- AI Model: Phi-3-Mini-4K-Instruct via Hugging Face
+   - Connecting Langchain and Huggingface: [ChatHuggingface class](https://python.langchain.com/api_reference/huggingface/chat_models/langchain_huggingface.chat_models.huggingface.ChatHuggingFace.html)
+
+
+## Getting Started
+Follow the steps below to install dependencies and run the chatbot.
+
+### Installation
+1. Clone the repository:  
+   `git clone https://github.com/your-username/ai-chatbot.git`
+   `cd ai-chatbot`
+
+2. Install dependencies:  
+   Run the following command to install all necessary dependencies:  
+   `pip3 install -r requirements.txt`
+
+### Run the chatbot
+After installing dependencies, start the chatbot by running:  
+`python3 chat.py`
+
+## Acknowledgements
+1. Model used: [Phi-3-Mini-4K-Instruct](https://huggingface.co/microsoft/Phi-3-mini-4k-instruct)
+2. Learning and Reference resources
+   - [Langchain](https://api.python.langchain.com/en/latest/langchain_api_reference.html)
+   - [ChatHuggingFace](https://python.langchain.com/api_reference/huggingface/chat_models/langchain_huggingface.chat_models.huggingface.ChatHuggingFace.html)
+   - [Chainlit](https://docs.chainlit.io/get-started/overview)
---
+++ bot.py
@@ -0,0 +1,60 @@
+import chainlit as cl
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+
+# AI Chat Implementation
+"""
+1. Define the HuggingFace endpoint
+2. Define the chat model
+4. Integrate chat invoking logic in the UI
+"""
+
+llm = HuggingFaceEndpoint(
+    repo_id="microsoft/Phi-3-mini-4k-instruct",
+    task="text-generation",
+    max_new_tokens=256,
+    do_sample=False,
+    repetition_penalty=1.03,
+)
+
+chat = ChatHuggingFace(llm=llm, verbose=True)
+
+# UI Implementation
+"""
+1. Define what the bot should say when the chat starts
+2. Define what the bot should say when the chat ends (OPTIONAL)
+3. Maintain a user session to store chat history for context
+4. Invoke chat, send user prompt, get a response and send it to UI
+"""
+
+START_STATEMENT = "Hello! I am a helpful AI assistant. How can I assist you today?"
+EXIT_STATEMENT = "The session has ended due to inactivity. New session created."
+
+@cl.on_chat_start
+async def handle_chat_start():
+    system_prompt = SystemMessage(content="You are a helpful AI assistant.")
+
+    cl.user_session.set("username", "Guest")
+    cl.user_session.set("chat_history", [system_prompt])
+
+    await cl.Message(content=START_STATEMENT).send()
+
+@cl.on_message
+async def handle_user_prompt(message: cl.Message):
+    #  convert user prompt into a standardised human message
+    user_prompt = HumanMessage(content=message.content)
+
+    # get chat_history for this session
+    chat_history = cl.user_session.get("chat_history")
+    chat_history.append(user_prompt)
+
+    #  invoke chat and get a response from the ai model
+    response = await chat.ainvoke(chat_history)
+
+    # update chat history  
+    chat_history.append(AIMessage(content=response.content))
+    
+    #  send it back to the user through the UI
+    await cl.Message(content=response.content).send()
+
+
